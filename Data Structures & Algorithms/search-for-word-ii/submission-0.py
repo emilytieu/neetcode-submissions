cecad23f7559d77005da22c3a928cee63af508c5
Emily Tieu <144379852+emilytieu@users.noreply.github.com>
2026-07-19 11:32:54 -0500

    Add: search-for-word-ii - submission-0

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-0.py
@@ -0,0 +1,48 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False
+
+    def insert(self, word):
+        node = self
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode()
+            node = node.children[char]
+        node.endOfWord = True
+
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        root = TrieNode()
+        for w in words:
+            root.insert(w)
+        
+        rows, cols = len(board), len(board[0])
+        result, visit = set(), set()
+
+        def dfs(r, c, node, word):
+            if (r < 0 or c < 0 
+                or r == rows or c == cols
+                or (r, c) in visit 
+                or board[r][c] not in node.children):
+                return
+
+            visit.add((r, c))
+            node = node.children[board[r][c]]
+            word += board[r][c]
+            if node.endOfWord:
+                result.add(word)
+            
+            dfs(r + 1, c, node, word)
+            dfs(r - 1, c, node, word)
+            dfs(r, c + 1, node, word)
+            dfs(r, c - 1, node, word)
+            visit.remove((r, c))
+
+        for r in range(rows):
+            for c in range(cols):
+                dfs(r, c, root, "")
+        
+        return list(result)
+
